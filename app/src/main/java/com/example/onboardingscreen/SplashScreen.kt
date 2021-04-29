9269473060da6cd3d622178a02123b959c74bb29
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-04-29 19:39:12 +0300

    partial

--- app/src/main/java/com/example/onboardingscreen/SplashScreen.kt
+++ app/src/main/java/com/example/onboardingscreen/SplashScreen.kt
@@ -1,16 +1,23 @@
 package com.example.onboardingscreen
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class SplashScreen : Fragment() {
 
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
+
+        Handler().postDelayed({
+            findNavController().navigate(R.id.action_splashScreen_to_viewPagerFragment)
+        },3000)
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
     }
---
+++ app/src/main/java/com/example/onboardingscreen/ViewPagerFragment.kt
@@ -0,0 +1,41 @@
+package com.example.onboardingscreen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager2.widget.ViewPager2
+import com.example.onboardingscreen.screens.FirstScreen
+import com.example.onboardingscreen.screens.SecondScreen
+import com.example.onboardingscreen.screens.ThirdScreen
+
+class ViewPagerFragment : Fragment() {
+private lateinit var viewPager: ViewPager2
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        viewPager = view.findViewById(R.id.viewPager2)
+
+        val fragmentList = arrayListOf<Fragment>(
+                FirstScreen(),
+                SecondScreen(),
+                ThirdScreen()
+        )
+
+
+        val adapter = ViewPagerAdapter(
+                fragmentList,
+                requireActivity().supportFragmentManager,
+                lifecycle
+        )
+
+        viewPager.adapter = adapter
+
+        return view
+    }
+
+}
